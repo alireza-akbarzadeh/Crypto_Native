1f349440d91743efa394cb58bc189ddd7be8ea1b
AlirezaDev951 <devtools95@gmail.com>
2022-04-16 19:03:13 +0430

    inital kms

---
+++ components/index.js
@@ -0,0 +1,12 @@
+export { default as HomeHeader } from "./HomeHeader";
+export { default as FocusStatusBar } from "./FocusStatusBar";
+export { default as NFTCard } from "./NFTCard";
+export {
+  EndDate,
+  EthPrice,
+  ImgCmp,
+  NFTTitle,
+  People,
+  SubInfo,
+} from "./SubInfo";
+export { CircleButton, RectButton } from "./Button";
